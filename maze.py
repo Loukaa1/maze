14ace4f9fc63958dc5afd8f230257352e7cdf35e
Louka BRARD <loukabrard5@gmail.com>
2025-02-28 14:52:55 +0100

    fin court chemin en rouge css

--- maze.py
+++ maze.py
@@ -1,9 +1,14 @@
 from jinja2 import Environment, FileSystemLoader
 import random
+import matplotlib.pyplot as plt
+import networkx as nx
+# import pygraphviz
 
 class Cellule:
-    def __init__(self, murNord, murEst, murSud, murOuest):
+    def __init__(self, murNord, murEst, murSud, murOuest, x, y):
         self.murs={'N':murNord,'E':murEst, 'S':murSud,'O':murOuest}
+        self.x = x
+        self.y = y
 
 #    def __repr__(self):
 #        return str([v for v in self.murs.values()])[1:-1]
@@ -21,8 +26,12 @@ class Labyrinthe:
         self.largeur = largeur
         self.grille=self.construire_grille(hauteur, largeur)
         self.creer_labyrinthe(0, 0, hauteur, largeur, hasard = hasard)
+        self.graphe = None
+        self.court_chemin = []
+        self.generer_graphe()
         self.generer_html()
-       
+
+    
     def __repr__(self):
         return str("\n".join([str(ligne) for ligne in self.grille]))
 
@@ -39,10 +48,10 @@ class Labyrinthe:
             table: un tableau de tableaux
         """
         grille = []
-        for _ in range(hauteur):
+        for j in range(hauteur):
             ligne = []
-            for _ in range(largeur):
-                cellule = Cellule(True, True, True, True)
+            for i in range(largeur):
+                cellule = Cellule(True, True, True, True, x=i, y=j)
                 ligne.append(cellule)
             grille.append(ligne)
         return grille
@@ -56,7 +65,7 @@ class Labyrinthe:
             c2_lig (int): le numéro de ligne de la cellule 2
             c2_col (int): le numéro de colonne de la cellule 2
         """
-        print(f"creer_passage(c1_l={c1_lig}, c1_c={c1_col}, c2_l={c2_lig}, c2_c={c2_col})")
+        #print(f"creer_passage(c1_l={c1_lig}, c1_c={c1_col}, c2_l={c2_lig}, c2_c={c2_col})")
         cellule1 = self.grille[c1_lig][c1_col]
         cellule2 = self.grille[c2_lig][c2_col]
         # cellule2 au Nord de cellule1
@@ -76,7 +85,7 @@ class Labyrinthe:
             cellule1.murs['E'] = False
             cellule2.murs['O'] = False
         
-    def creer_labyrinthe(self, ligne, colonne, hauteur, largeur, hasard = False):
+    def creer_labyrinthe(self, ligne, colonne, hauteur, largeur, hasard = False, nb_portes = 3):
         if hauteur == 1 : # Cas de base
             for k in range(colonne, colonne + largeur-1):
                 self.creer_passage(ligne, k, ligne, k+1)
@@ -89,8 +98,15 @@ class Labyrinthe:
                 if not hasard :
                     self.creer_passage(ligne+hauteur//2, colonne, ligne+hauteur//2-1, colonne)
                 else :
-                    position = random.randint(colonne, colonne+largeur-1)
-                    self.creer_passage(ligne+hauteur//2, position , ligne+hauteur//2-1, position)
+                    #plusieurs portes en fonction de nb_portes
+                    if nb_portes < hauteur:
+                        for n in range(nb_portes) :
+                            position = random.randint(colonne, colonne+largeur-1)
+                            self.creer_passage(ligne+hauteur//2, position , ligne+hauteur//2-1, position)
+                    else :
+                        position = random.randint(colonne, colonne+largeur-1)
+                        self.creer_passage(ligne+hauteur//2, position , ligne+hauteur//2-1, position)
+                
                 # ------ RECURSIF
                 #partie Nord
                 self.creer_labyrinthe(ligne, colonne, hauteur//2, largeur, hasard = hasard)
@@ -102,13 +118,36 @@ class Labyrinthe:
                     self.creer_passage(ligne, colonne+largeur//2, ligne, colonne+largeur//2-1)
                 else :
                     position = random.randint(ligne, ligne+hauteur-1)
-                    print(position)
                     self.creer_passage(position, colonne+largeur//2, position, colonne+largeur//2-1)
                 # ------ RECURSIF
                 #partie Ouest
                 self.creer_labyrinthe(ligne, colonne, hauteur, largeur//2, hasard = hasard)
                 #partie Est
                 self.creer_labyrinthe(ligne, colonne + largeur//2, hauteur, largeur-largeur//2, hasard = hasard)
+                
+    def generer_graphe(self) :
+        G1 = nx.Graph()
+        self.graphe = G1
+        for i in range(len(self.grille)) :
+            for j in range(len(self.grille[i])) :
+                G1.add_node((i, j))
+        for j, ligne in enumerate(self.grille) :
+            for i, cellule in enumerate(ligne) :
+                mur = cellule.murs
+                if not mur['E'] :
+                    G1.add_edge((i, j), (i+1, j))
+                if not mur['S'] :
+                    G1.add_edge((i, j), (i, j+1))
+                    
+    def plus_court_chemin(self):
+        entree = list(self.graphe.nodes)[0]
+        sortie = list(self.graphe.nodes)[-1]
+        chemin = nx.shortest_path(self.graphe, entree, sortie)
+        self.court_chemin = chemin
+        return chemin
+                  
+                
+            
                 
     def generer_html(self):
         env = Environment(loader=FileSystemLoader('.'))
@@ -117,10 +156,27 @@ class Labyrinthe:
         html_rendu = 'maze.html'
         with open(html_rendu, 'w') as hr:
             hr.write(html_template)
+            
+    def show_graphe(self) :
+        options = {
+            "font_size": 15,
+            "node_size": 800,
+            "node_color": "#A0CBE2",
+            "edgecolors": "tab:black",
+            "alpha": 0.9,
+            "linewidths": 2,
+            "width": 2,
+        }
+        plt.figure(figsize=(5,5), facecolor='lightskyblue')
+        pos = nx.nx_agraph.graphviz_layout(self.graphe)
+        nx.draw(self.graphe, pos, with_labels=True)
+        plt.show()
+
+
         
     
 
 #cellule = Cellule(murNord=True, murEst=False, murSud=True, murOuest=True)
-h=16
-w=16
+h=20
+w=20
 labyrinthe = Labyrinthe(h, w, hasard = True)
